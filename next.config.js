3a1a1ebe9aa564673faa6b6c825c41acb5688e8e
Szymon Swist <szymonswist97@gmail.com>
2022-07-28 18:30:33 +0200

    Fix authentication on production

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-  },
 }
 
 module.exports = nextConfig
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -8,4 +8,5 @@ export default NextAuth({
       clientSecret: process.env.CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET as string
 })
